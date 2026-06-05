d113759b7520a4aa48dc17fac9411824e0959ea7
Atulya Saurav Abhisanchit <119062180+Atulyasaurav@users.noreply.github.com>
2026-06-05 14:14:41 +0530

    Update Docker credentials in dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String client, String server){
   withCredentials([usernamePassword(
-    "credentialsId": "dockerHubCred",
+    "credentialsId": "dockerUserCred",
     passwordVariable: "dockerHubPass",
     usernameVariable: "dockerHubUser"
   )]){
