61ac635d5114d08a6f02c4bd88db9a5e9ff53afb
Atulya Saurav Abhisanchit <119062180+Atulyasaurav@users.noreply.github.com>
2026-06-05 13:55:50 +0530

    Refactor dockerBuild to include Docker Hub credentials
    Updated the dockerBuild function to use credentials for Docker Hub.

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,10 @@
-def call(String client, String server, String username){
-  sh "cd ${client} && docker build -t ${username}/${client} ."
-  sh "cd ${server} && docker build -t ${username}/${server} ."
+def call(String client, String server){
+  withCredentials([usernamePassword(
+    credentialsId: "dockerHubCred",
+    passwordVariable: "dockerHubPass",
+    usernameVariable: "dockerHubUser"
+  )]){
+  sh "cd ${client} && docker build -t ${dockerHubUser}/${client} ."
+  sh "cd ${server} && docker build -t ${dockerHubUser}/${server} ."
+  }
 }
